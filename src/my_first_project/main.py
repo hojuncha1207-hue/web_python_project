a208cd5612cfa4c521d008274ff27caa060fca3d
hojuncha1207-hue <hojuncha1207@gmail.com>
2025-11-17 15:18:46 +0900

    Update main.py

--- src/my_first_project/main.py
+++ src/my_first_project/main.py
@@ -127,18 +127,6 @@ class EEStudent_Info:
             score = int((matched_count / total_attended_count) * 100)
             
         return score, matched_classes, sorted(unmatched_classes)
-    def get_recommended_url(self)->str:
-        for interest_key in INTEREST_COURSE_MAP.keys():
-            if self.interested == interest_key:
-                url_map={
-                    "회로":"https://linkareer.com/search?q=%ED%9A%8C%EB%A1%9C&page=1",
-                    "반도체":"https://linkareer.com/search?q=%EB%B0%98%EB%8F%84%EC%B2%B4&page=1",
-                    "신호":"https://linkareer.com/search?q=%EC%8B%A0%ED%98%B8+%EC%B2%98%EB%A6%AC&page=1",
-                    "임베디드":"https://linkareer.com/search?q=%EC%9E%84%EB%B2%A0%EB%94%94%EB%93%9C&page=1",
-                    "AI": "https://linkareer.com/search?q=AI&page=1",
-                }
-                return url_map.get(interest_key,"")
-        return ""
 
     def get_recommended_url(self)->str:
         for interest_key in INTEREST_COURSE_MAP.keys():
@@ -149,9 +137,10 @@ class EEStudent_Info:
                     "신호":"https://linkareer.com/search?q=%EC%8B%A0%ED%98%B8+%EC%B2%98%EB%A6%AC&page=1",
                     "임베디드":"https://linkareer.com/search?q=%EC%9E%84%EB%B2%A0%EB%94%94%EB%93%9C&page=1",
                     "머신러닝":"https://linkareer.com/search?q=%EB%A8%B8%EC%8B%A0%EB%9F%AC%EB%8B%9D&page=1",
-                    "AI":"https://linkareer.com/search?q=AI&page=1"
+                    "AI":"https://linkareer.com/search?q=AI&page=1",
                     "로봇":"https://linkareer.com/search?q=%EB%A1%9C%EB%B4%87&page=1"
                 }
                 return url_map.get(interest_key,"")
         return ""
+
 
